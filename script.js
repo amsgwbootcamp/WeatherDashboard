a4f61a921bb337dbf524b0657441e6e0a73e379b
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-03-14 22:15:37 -0400

    All fields excepts for 5 day forecast

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Here we run our AJAX call to the OpenWeatherMap API
+var searchLat;
+var searchLon;
+var currentDate;
+
 function getInfo(searchCity)
 {
+    currDate = moment().format('MMMM Do YYYY');
     var APIKey = "9c487a7f9aa142c820a7b4aa2d194f2f";
-    //var searchCity = $("#search").val();
+    
     // Here we are building the URL we need to query the database
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
             + searchCity + "&appid=" + APIKey;
@@ -15,27 +20,36 @@ function getInfo(searchCity)
     // We store all of the retrieved data inside of an object called "response"
     .then(function(response) {
   
-        // Log the queryURL
-        console.log(queryURL);
-        // Log the resulting object
-        console.log(response);
-  
         // Transfer content to HTML
-        $(".displayCity").text(searchCity);
+        $(".displayCity").text("Weather for: " + searchCity + " on " + currentDate);
         $(".displayHumidity").text("Humidity: " + response.main.humidity  + "%");
         $(".displayWindSpeed").text("Wind Speed: " + response.wind.speed  + " MPH");
-        //$(".displayUVIndex").text("UV Index: " + response.name);
-          
         // Convert the temp to fahrenheit
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         var degreeSymbol = "\xB0";
         // add temp content to html
         $(".displayTemp").text("Temperature: " + tempF.toFixed(2) + " " + degreeSymbol + "F");
-          
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
+        searchLat = response.coord.lat;
+        searchLon = response.coord.lon;
+        getUVIndex(searchLon,searchLat);
+    }); 
+}
+
+function getUVIndex(cityLat, cityLon)
+{
+    var APIKey = "9c487a7f9aa142c820a7b4aa2d194f2f";
+    //var searchCity = $("#search").val();
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?&appid=" 
+            + APIKey + "&lat=" + cityLat + "&lon=" + cityLon + "&cnt=1";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+        $(".displayUVIndex").text("UV Index: " + response[0].value);
     }); 
 }
 
@@ -78,11 +92,17 @@ function getInfo(searchCity)
 } */
 
 $(".searchButton").on("click", function() {
+    
     var str = $("#search").val();  
-//    alert("button has been clicked");
-//    alert(str);
     $(".container").append("<div>"+ str + "</div>");
+
     getInfo(str);
     //getFiveDay(str);
     $("#search").val("");
 });
+
+$(document).ready(function(){
+    
+    currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+      
+  });
