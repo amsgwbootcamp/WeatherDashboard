15ad0985129078d3d71c4fd87417d9ba3e6ad183
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-03-17 21:50:01 -0400

    All final modification have been completed.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WeatherDashboard
 This will be a website that will show the five day forecast for a given city.  
+
+There is a button that will clear local storage.  When you click the button, the next time you click the reload button, no new city will loaded and the screen will reset.  If you click the reload button for the browser, the information will still be displayed for the city that was last displayed.  
+
+On the left side, when you enter a city and click the search button, the information will appear on the right.  The city that was displayed will be seen in a list underneath the input box.  You will be able to click on any of the city names and the information will be redisplayed on the right.    
+
+The top of the right side, you will see the city, date and time displayed.  Underneath that, the temperature, an icon representation, humidity, wind speed and the uv index which is color coded will be displayed to the user.  Red is very bad, yellow is cautionary and green is fine.  At the bottom of the right side, you will see a five day forecast which will display the date, an icon representation, the temperature and the humidity which is based on the forecast for noon of each day displayed.
--- script.js
+++ script.js
@@ -1,10 +1,138 @@
-// Here we run our AJAX call to the OpenWeatherMap API
-// Here we run our AJAX call to the OpenWeatherMap API
 var searchLat;
 var searchLon;
 var currentDate;
 var counter = 1;
 
+//
+// Function to process the city button that was clicked from the list.  
+//
+function cityButtonClickListener(event) 
+{
+    var a = this.event.target.value;
+    getInfo(a);
+    getFiveDay(a);
+    localStorage.setItem("lastCity",a);
+}
+//
+// Function to create a new city button that was entered in the search box.  
+//
+function createCityButton(str) {
+var cityButton = $("<input/>").attr({
+             type: "button",
+             id: "btn"+counter,
+             value: str,
+             onclick: "cityButtonClickListener()"
+        });
+        cityButton.css("width","100%");
+        cityButton.css("border","none");
+        cityButton.css("text-align","left");
+        counter = counter + 1;
+        $(".container").append(cityButton);
+        $(".container").append("<br>");
+        localStorage.setItem("lastCity",str);
+}
+
+//
+// Function to get all of the information needed for the five day forecast.  
+//
+function getFiveDay(searchCity)
+{
+    var APIKey = "9c487a7f9aa142c820a7b4aa2d194f2f";
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" 
+            + searchCity + "&appid=" + APIKey;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+  
+        // Transfer content to HTML
+        // Entry 1
+        $(".fTitle").text("5 Day Forecast:");
+        $(".fTitle").css("visibility","visible");
+        var mDate1 = response.list[0].dt_txt;
+        var dMonth1 = mDate1.substring(5,7);
+        var dDay1 = mDate1.substring(8,10);
+        var dYear1 = mDate1.substring(0,4);
+        var dDate1 = dMonth1 + "/" + dDay1 + "/" + dYear1;
+        $(".displayDate1").text(dDate1);
+        var tempF1 = (response.list[3].main.temp - 273.15) * 1.80 + 32;
+        var degreeSymbol = "\xB0";
+        $(".displayTemp1").text("Temperature: " + tempF1.toFixed(2)  + " " + degreeSymbol + "F");
+        var iconcode = response.list[3].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $("#wicon1").attr("src",iconurl);
+        $("#wicon1").css("visibility","visible");
+        $(".displayHumidity1").text("Humidity: " + response.list[0].main.humidity + "%");
+        // Entry 2
+        var mDate2 = response.list[8].dt_txt;
+        var dMonth2 = mDate2.substring(5,7);
+        var dDay2 = mDate2.substring(8,10);
+        var dYear2 = mDate2.substring(0,4);
+        var dDate2 = dMonth2 + "/" + dDay2 + "/" + dYear2;
+        $(".displayDate2").text(dDate2);
+        var tempF2 = (response.list[11].main.temp - 273.15) * 1.80 + 32;
+        var degreeSymbol = "\xB0";
+        $(".displayTemp2").text("Temperature: " + tempF2.toFixed(2)  + " " + degreeSymbol + "F");
+        var iconcode = response.list[11].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $("#wicon2").attr("src",iconurl);
+        $("#wicon2").css("visibility","visible");
+        $(".displayHumidity2").text("Humidity: " + response.list[8].main.humidity + "%");
+        // Entry 3
+        var mDate3 = response.list[16].dt_txt;
+        var dMonth3 = mDate3.substring(5,7);
+        var dDay3 = mDate3.substring(8,10);
+        var dYear3 = mDate3.substring(0,4);
+        var dDate3 = dMonth3 + "/" + dDay3 + "/" + dYear3;
+        $(".displayDate3").text(dDate3);
+        var tempF3 = (response.list[19].main.temp - 273.15) * 1.80 + 32;
+        var degreeSymbol = "\xB0";
+        $(".displayTemp3").text("Temperature: " + tempF3.toFixed(2)  + " " + degreeSymbol + "F");
+        var iconcode = response.list[19].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $("#wicon3").attr("src",iconurl);
+        $("#wicon3").css("visibility","visible");
+        $(".displayHumidity3").text("Humidity: " + response.list[16].main.humidity + "%");
+        // Entry 4
+        var mDate4 = response.list[24].dt_txt;
+        var dMonth4 = mDate4.substring(5,7);
+        var dDay4 = mDate4.substring(8,10);
+        var dYear4 = mDate4.substring(0,4);
+        var dDate4 = dMonth4 + "/" + dDay4 + "/" + dYear4;
+        $(".displayDate4").text(dDate4);
+        var tempF4 = (response.list[27].main.temp - 273.15) * 1.80 + 32;
+        var degreeSymbol = "\xB0";
+        $(".displayTemp4").text("Temperature: " + tempF3.toFixed(2)  + " " + degreeSymbol + "F");
+        var iconcode = response.list[27].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $("#wicon4").attr("src",iconurl);
+        $("#wicon4").css("visibility","visible");
+        $(".displayHumidity4").text("Humidity: " + response.list[27].main.humidity + "%");
+        // Entry 5
+        var mDate5 = response.list[32].dt_txt;
+        var dMonth5 = mDate5.substring(5,7);
+        var dDay5 = mDate5.substring(8,10);
+        var dYear5 = mDate5.substring(0,4);
+        var dDate5 = dMonth5 + "/" + dDay5 + "/" + dYear5;
+        $(".displayDate5").text(dDate5);
+        var tempF4 = (response.list[35].main.temp - 273.15) * 1.80 + 32;
+        var degreeSymbol = "\xB0";
+        $(".displayTemp5").text("Temperature: " + tempF3.toFixed(2)  + " " + degreeSymbol + "F");
+        var iconcode = response.list[35].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $("#wicon5").attr("src",iconurl);
+        $("#wicon5").css("visibility","visible");
+        $(".displayHumidity5").text("Humidity: " + response.list[35].main.humidity + "%");
+        
+    }); 
+} 
+//
+// Function to get information for the current day forecast.  
+//
 function getInfo(searchCity)
 {
     currDate = moment().format('MMMM Do YYYY');
@@ -14,7 +142,6 @@ function getInfo(searchCity)
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
             + searchCity + "&appid=" + APIKey;
 
-    //alert("We are looking for " + searchCity);
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -39,10 +166,11 @@ function getInfo(searchCity)
         searchLon = response.coord.lon;
         getUVIndex(searchLon,searchLat);
         $(".fTitle").css("visibility","visible");
-    //    getFiveDay(searchCity);
     }); 
 }
-
+//
+// Function to get the UV Index and color code it based on the index level.  
+//
 function getUVIndex(cityLon, cityLat)
 {
     var APIKey = "9c487a7f9aa142c820a7b4aa2d194f2f";
@@ -84,144 +212,38 @@ function getUVIndex(cityLon, cityLat)
         }
     }); 
 }
-
-function getFiveDay(searchCity)
+//
+// Event listeners.  
+//
+$(".clearButton").on("click", function()
 {
-    var APIKey = "9c487a7f9aa142c820a7b4aa2d194f2f";
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" 
-            + searchCity + "&appid=" + APIKey;
-
-    alert("We are looking for " + searchCity);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
-  
-        // Transfer content to HTML
-        // Entry 1
-        $(".fTitle").text("5 Day Forecast:");
-        $(".fTitle").css("visibility","visible");
-        var mDate1 = response.list[0].dt_txt;
-        var dMonth1 = mDate1.substring(5,7);
-        var dDay1 = mDate1.substring(8,10);
-        var dYear1 = mDate1.substring(0,4);
-        var dDate1 = dMonth1 + "/" + dDay1 + "/" + dYear1;
-        $(".displayDate1").text(dDate1);
-        var tempF1 = (response.list[0].main.temp - 273.15) * 1.80 + 32;
-        var degreeSymbol = "\xB0";
-        $(".displayTemp1").text("Temperature: " + tempF1.toFixed(2)  + " " + degreeSymbol + "F");
-        var iconcode = response.list[0].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        $("#wicon1").attr("src",iconurl);
-        $("#wicon1").css("visibility","visible");
-        $(".displayHumidity1").text("Humidity: " + response.list[0].main.humidity + "%");
-        // Entry 2
-        var mDate2 = response.list[8].dt_txt;
-        var dMonth2 = mDate2.substring(5,7);
-        var dDay2 = mDate2.substring(8,10);
-        var dYear2 = mDate2.substring(0,4);
-        var dDate2 = dMonth2 + "/" + dDay2 + "/" + dYear2;
-        $(".displayDate2").text(dDate2);
-        var tempF2 = (response.list[8].main.temp - 273.15) * 1.80 + 32;
-        var degreeSymbol = "\xB0";
-        $(".displayTemp2").text("Temperature: " + tempF2.toFixed(2)  + " " + degreeSymbol + "F");
-        var iconcode = response.list[8].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        $("#wicon2").attr("src",iconurl);
-        $("#wicon2").css("visibility","visible");
-        $(".displayHumidity2").text("Humidity: " + response.list[8].main.humidity + "%");
-        // Entry 3
-        var mDate3 = response.list[16].dt_txt;
-        var dMonth3 = mDate3.substring(5,7);
-        var dDay3 = mDate3.substring(8,10);
-        var dYear3 = mDate3.substring(0,4);
-        var dDate3 = dMonth3 + "/" + dDay3 + "/" + dYear3;
-        $(".displayDate3").text(dDate3);
-        var tempF3 = (response.list[16].main.temp - 273.15) * 1.80 + 32;
-        var degreeSymbol = "\xB0";
-        $(".displayTemp3").text("Temperature: " + tempF3.toFixed(2)  + " " + degreeSymbol + "F");
-        var iconcode = response.list[16].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        $("#wicon3").attr("src",iconurl);
-        $("#wicon3").css("visibility","visible");
-        $(".displayHumidity3").text("Humidity: " + response.list[16].main.humidity + "%");
-        // Entry 4
-        var mDate4 = response.list[24].dt_txt;
-        var dMonth4 = mDate4.substring(5,7);
-        var dDay4 = mDate4.substring(8,10);
-        var dYear4 = mDate4.substring(0,4);
-        var dDate4 = dMonth4 + "/" + dDay4 + "/" + dYear4;
-        $(".displayDate4").text(dDate4);
-        var tempF4 = (response.list[24].main.temp - 273.15) * 1.80 + 32;
-        var degreeSymbol = "\xB0";
-        $(".displayTemp4").text("Temperature: " + tempF3.toFixed(2)  + " " + degreeSymbol + "F");
-        var iconcode = response.list[24].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        $("#wicon4").attr("src",iconurl);
-        $("#wicon4").css("visibility","visible");
-        $(".displayHumidity4").text("Humidity: " + response.list[32].main.humidity + "%");
-        // Entry 5
-        var mDate5 = response.list[32].dt_txt;
-        var dMonth5 = mDate5.substring(5,7);
-        var dDay5 = mDate5.substring(8,10);
-        var dYear5 = mDate5.substring(0,4);
-        var dDate5 = dMonth5 + "/" + dDay5 + "/" + dYear5;
-        $(".displayDate5").text(dDate5);
-        var tempF4 = (response.list[32].main.temp - 273.15) * 1.80 + 32;
-        var degreeSymbol = "\xB0";
-        $(".displayTemp5").text("Temperature: " + tempF3.toFixed(2)  + " " + degreeSymbol + "F");
-        var iconcode = response.list[32].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        $("#wicon5").attr("src",iconurl);
-        $("#wicon5").css("visibility","visible");
-        $(".displayHumidity5").text("Humidity: " + response.list[32].main.humidity + "%");
-        
-    }); 
-} 
+    localStorage.clear();
+});
 
 $(".searchButton").on("click", function() {
 
     var str = $("#search").val();
     getInfo(str);
     getFiveDay(str);
-    test(str);
+    createCityButton(str);
     $("#search").val("");  
 });
-
-function newButtonClickListener(event) {
-    alert(this.event.target.id);
-    var a = this.event.target.value;
-    alert("You have pushed the following: " + a);
-    getInfo(a);
-    getFiveDay(a);
-
-}
-function test(str) {
-var r = $('<input/>').attr({
-             type: "button",
-             id: "btn"+counter,
-             value: str,
-             onclick: "newButtonClickListener()"
-        });
-        counter = counter + 1;
-        $(".container").append(r);
-        $(".container").append("<br>");
-}
-
+//
+// Initial setup of all buttons that are used in the html.  
+//
 $(document).ready(function(){
     
     currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     var lastSearch = localStorage.getItem("lastCity");
     if (lastSearch !== null)
     {
-        alert("last city searched")
+        getInfo(lastSearch);
+        getFiveDay(lastSearch);
+        $("#search").val("");  
     }
     else
     {
-        $(".displayCity").text("Please enter a city to search for");
+        $(".displayCity").text("Please enter a city to search for on the left");
         $(".displayUVIndex").css("visiblity","hidden");
         $(".fTitle").css("visibility","hidden");
         $("#wicon").css("visibility","hidden");
